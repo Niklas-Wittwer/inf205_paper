36db8659b7e0a0e6d0a2599c5615b2162eaff2ff
Niklas-Wittwer <niklas.wittwer@nmbu.no>
2022-12-03 15:32:04 +0100

    -_-

--- utils.h
+++ utils.h
@@ -8,7 +8,7 @@ Header file for functions related to our algorithm
 
 void create_pbc(Box origin, std::vector<std::vector<std::vector<Box>>>& pbc, int dim);
 
-void monte_carlo(Box box1){};
-void move_spheres(Box optimal_pos, int n){};
+void monte_carlo(std::vector<std::vector<std::vector<Box>>>& pbc);
+void move_spheres(Box optimal_pos, int n);
 
 #endif
