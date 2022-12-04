cdff322a5fe8c0cb8afeacf4a675072db5586b4e
Niklas-Wittwer <niklas.wittwer@nmbu.no>
2022-12-04 22:00:32 +0100

    Minor changes

--- utils.h
+++ utils.h
@@ -10,5 +10,5 @@ void create_pbc(Box origin, std::vector<std::vector<std::vector<Box>>>& pbc, int
 
 void monte_carlo(std::vector<std::vector<std::vector<Box>>>& pbc, double cube_len);
 void move_spheres(std::vector<std::vector<std::vector<Box>>>& pbc, int start, int end, double cube_len);
-
+double rand_num_gen(double lower_lim, double upper_lim);
 #endif
